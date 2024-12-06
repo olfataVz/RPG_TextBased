0099f97ec4901e93025c17ac87ae79da4a1a5775
olfa <olfatavz@gmail.com>
2024-12-06 18:56:36 +0800

    Session 3

--- src/Wizard.java
+++ src/Wizard.java
@@ -139,7 +139,6 @@ public class Wizard {
             } else {
                 animationTimer.stop();
                 playSkillAnimation();
-                playIdleAnimation();
             }
         });
         animationTimer.start();
